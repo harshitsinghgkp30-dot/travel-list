43d08efb88a5055c8d1f0db08aaa737fd48527b7
faraway <far>
2026-09-07 20:30:22 +0530

    Show packing list statistics

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -113,10 +113,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em> you have x items on your list , and you already packed X (X%) </em>
+      <em>
+        {percentage === 100
+          ? "you are ready to go!"
+          : `you have ${numItems} items on your list , and you already packed${numPacked}(${percentage}%)`}
+      </em>
     </footer>
   );
 }
